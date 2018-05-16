3d7d46068c28e525214745698a46c85880266bb5
h4r3krsna <manju.sreedaran@gmail.com>
2018-05-15 18:30:33 -0700

    Student submission form with data fields; Form POST handler skeleton

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 require 'vendor/autoload.php';
 
+function ln() {
+    echo '<br>';
+}
+
 $f3 = \Base::instance();
 
 $f3->route('GET /',
@@ -11,6 +15,20 @@ $f3->route('GET /',
     $view = new View;
     echo $view->render('views/student_form.php');
   }
+);
+
+$f3->route('POST /',
+    function() {
+        // validate, sanitize and save to DB
+
+        // if (allOK) {
+        // return a confirmation
+        echo 'Thank you for your submission!';
+        ln();
+        echo $_POST['first-name'];
+
+        //} else { return ERRORS; }
+    }
 );
 
 $f3->run();
--- views/student_form.php
+++ views/student_form.php
@@ -27,7 +27,46 @@
         <h1>Prior Learning Assessment Request Form</h1>
 
         <div id="wrapper">
+            <form action="/" method="POST">
+                <div class="form-group">
+                    <label for="username">Student ID#:</label>
+                    <input type="text" class="form-control" id="student-id" name="student-id" placeholder="XXXXXXXXX" required>
+                    <label for="first-name">First Name:</label>
+                    <input type="text" class="form-control" id="first-name" name="first-name" placeholder="" required>
+                    <label for="last-name">Last Name:</label>
+                    <input type="text" class="form-control" id="last-name" name="last-name" placeholder="" required>
+                    <label for="email">Email:</label>
+                    <input type="text" class="form-control" id="first-name" name="first-name" placeholder="" required>
 
+                </div>
+                <div class="form-group">
+                    <label for="internship-title">Internship Title:</label>
+                    <input type="text" class="form-control" id="internship-title" name="internship-title" placeholder="" required>
+                    <label for="company">Company:</label>
+                    <input type="text" class="form-control" id="company" name="company" placeholder="" required>
+                    <label for="start-date">Start Date:</label>
+                    <input type="text" class="form-control" id="start-date" name="start-date" placeholder="" required>
+                    <label for="end-date">End Date:</label>
+                    <input type="text" class="form-control" id="end-date" name="end-date" placeholder="" required>
+                    <label for="hours-worked">Hours Worked:</label>
+                    <input type="text" class="form-control" id="hours-worked" name="hours-worked" placeholder="" required>
+                </div>
+                <div class="form-group">
+                    <label for="supervisor-name">Supervisor Name:</label>
+                    <input type="text" class="form-control" id="supervisor-name" name="supervisor-name" placeholder="" required>
+                    <label for="supervisor-title">Supervisor Title:</label>
+                    <input type="text" class="form-control" id="supervisor-title" name="supervisor-title" placeholder="" required>
+                    <label for="supervisor-email">Supervisor Email:</label>
+                    <input type="text" class="form-control" id="supervisor-email" name="supervisor-email" placeholder="" required>
+                    <label for="supervisor-phone">Supervisor Phone:</label>
+                    <input type="text" class="form-control" id="supervisor-phone" name="supervisor-phone" placeholder="" required>
+                </div>
+                <div class="form-group">
+                    <label for="duties-description">Description of Duties:</label>
+                    <textarea class="form-control" id="duties-description" name="duties-description" required></textarea>
+                </div>
+                <input type="submit" class="btn btn-primary" value="Submit">
+            </form>
         </div>
     </article>
 </div>
