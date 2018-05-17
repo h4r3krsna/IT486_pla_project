b6dd42ca9e0b71d67ed8a0a0d0d4c5ef1a91addd
h4r3krsna <manju.sreedaran@gmail.com>
2018-05-16 19:13:18 -0700

    form submission handler psuedocode; entry viewer handler stub

--- index.php
+++ index.php
@@ -10,8 +10,6 @@ $f3 = \Base::instance();
 
 $f3->route('GET /',
   function() {
-    //echo 'Welcome to the Prior Learning Assessment Request Form!';
-
     $view = new View;
     echo $view->render('views/student_form.php');
   }
@@ -20,16 +18,32 @@ $f3->route('GET /',
 $f3->route('POST /',
     function() {
         // validate, sanitize and save to DB
+        print_r($_POST);
+        ln();
+
+        /*
+         * INSERT INTO entries (studentid, firstname, lastname, email, internshiptitle, company, startdate,enddate,hoursworked,supervisorname,supervisortitle, supervisoremail,supervisorphone, descriptionofduties) VALUES ('123456789', 'first','last','email@mail.com','title','company',CURDATE(),CURDATE(),0,'supervisor','boss','boss@work.com','9876542310','duties duties duties duties duties duties duties duties');
+         */
 
         // if (allOK) {
-        // return a confirmation
+        // email the instructor with a link to the entry & return a confirmation to the student
+        // TODO: how to get id of last insert https://www.w3schools.com/php/php_mysql_insert_lastid.asp
+        // echo '<a href="http://msreedaran.greenrivertech.net/plaform/entries/1
         echo 'Thank you for your submission!';
-        ln();
-        print_r($_POST);
 
         //} else { return ERRORS; }
     }
 );
+
+$f3->route('GET /entries/@id',
+    function($f3, $params) {
+        $entryId = $params['id'];
+        $stmt = 'SELECT * FROM entries WHERE id=' . $entryId;
+        echo $stmt;
+
+        // if record found, show form with filled fields
+        // else show
+    });
 
 $f3->run();
 
