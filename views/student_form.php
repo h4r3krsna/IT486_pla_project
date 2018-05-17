a1355191c297b526d3fbfab2b9cea31a8bbc6de4
h4r3krsna <manju.sreedaran@gmail.com>
2018-05-16 19:53:17 -0700

    form formatting and validation enhancements

--- views/student_form.php
+++ views/student_form.php
@@ -11,57 +11,83 @@
 </head>
 <body>
 <div class="container">
-    <article>
-        <h1>Prior Learning Assessment Request Form</h1>
-        <div id="wrapper">
-            <form id="plaform" name="plaform" action="" onsubmit="return validateForm()" method="POST">
-                <div class="form-group">
-                    <label for="student-id">Student ID#:</label>
-                    <input type="text" class="form-control" id="student-id" name="student-id" placeholder="XXXXXXXXX" required>
-                    <label for="first-name">First Name:</label>
-                    <input type="text" class="form-control" id="first-name" name="first-name" placeholder="" required>
-                    <label for="last-name">Last Name:</label>
-                    <input type="text" class="form-control" id="last-name" name="last-name" placeholder="" required>
-                    <label for="student-email">Email:</label>
-                    <input type="email" class="form-control" id="student-email" name="student-email" placeholder="" required>
-                </div>
-                <div class="form-group">
-                    <label for="internship-title">Internship Title:</label>
-                    <input type="text" class="form-control" id="internship-title" name="internship-title" placeholder="" required>
-                    <label for="company">Company:</label>
-                    <input type="text" class="form-control" id="company" name="company" placeholder="" required>
-                    <label for="start-date">Start Date:</label>
-                    <input type="text" class="form-control" id="start-date" name="start-date" placeholder="YYYY-MM-DD" required>
-                    <label for="end-date">End Date:</label>
-                    <input type="text" class="form-control" id="end-date" name="end-date" placeholder="YYYY-MM-DD" required>
-                    <label for="hours-worked">Hours Worked:</label>
-                    <input type="text" class="form-control" id="hours-worked" name="hours-worked" placeholder="" required>
-                </div>
-                <div class="form-group">
-                    <label for="supervisor-name">Supervisor Name:</label>
-                    <input type="text" class="form-control" id="supervisor-name" name="supervisor-name" placeholder="" required>
-                    <label for="supervisor-title">Supervisor Title:</label>
-                    <input type="text" class="form-control" id="supervisor-title" name="supervisor-title" placeholder="" required>
-                    <label for="supervisor-email">Supervisor Email:</label>
-                    <input type="email" class="form-control" id="supervisor-email" name="supervisor-email" placeholder="" required>
-                    <label for="supervisor-phone">Supervisor Phone:</label>
-                    <input type="text" class="form-control" id="supervisor-phone" name="supervisor-phone" placeholder="XXX-XXX-XXXX" required>
-                </div>
-                <div class="form-group">
-                    <label for="duties-description">Description of Duties:</label>
-                    <textarea class="form-control" id="duties-description" name="duties-description" rows="8" required></textarea>
-                </div>
-                <input type="submit" class="btn btn-primary pull-right" value="Submit">
-            </form>
+    <h1>Prior Learning Assessment Request Form</h1>
+    <form id="plaform" name="plaform" action="" onsubmit="return validateForm()" method="POST">
+        <div class="form-group">
+            <fieldset>
+                <legend>Personal Identification</legend>
+                <label for="student-id">Student ID#:</label>
+                <input type="number" class="form-control" id="student-id" name="student-id" placeholder="XXXXXXXXX" required>
+                <label for="first-name">First Name:</label>
+                <input type="text" class="form-control" id="first-name" name="first-name" placeholder="" required>
+                <label for="last-name">Last Name:</label>
+                <input type="text" class="form-control" id="last-name" name="last-name" placeholder="" required>
+                <label for="student-email">Email:</label>
+                <input type="email" class="form-control" id="student-email" name="student-email" placeholder="" required>
+            </fieldset>
         </div>
-    </article>
+        <div class="form-group">
+            <fieldset>
+                <legend>Internship Information</legend>
+                <label for="internship-title">Internship Title:</label>
+                <input type="text" class="form-control" id="internship-title" name="internship-title" placeholder="" required>
+                <label for="company">Company:</label>
+                <input type="text" class="form-control" id="company" name="company" placeholder="" required>
+                <label for="start-date">Start Date:</label>
+                <input type="date" class="form-control" id="start-date" name="start-date" placeholder="" required>
+                <label for="end-date">End Date:</label>
+                <input type="date" class="form-control" id="end-date" name="end-date" placeholder="" required>
+                <label for="hours-worked">Hours Worked:</label>
+                <input type="number" class="form-control" id="hours-worked" name="hours-worked" placeholder="" required>
+            </fieldset>
+        </div>
+        <div class="form-group">
+            <fieldset>
+                <legend>Supervisor Details</legend>
+                <label for="supervisor-name">Supervisor Name:</label>
+                <input type="text" class="form-control" id="supervisor-name" name="supervisor-name" placeholder="" required>
+                <label for="supervisor-title">Supervisor Title:</label>
+                <input type="text" class="form-control" id="supervisor-title" name="supervisor-title" placeholder="" required>
+                <label for="supervisor-email">Supervisor Email:</label>
+                <input type="email" class="form-control" id="supervisor-email" name="supervisor-email" placeholder="" required>
+                <label for="supervisor-phone">Supervisor Phone:</label>
+                <input type="text" class="form-control" id="supervisor-phone" name="supervisor-phone" placeholder="XXX-XXX-XXXX" required>
+            </fieldset>
+        </div>
+        <div class="form-group">
+            <fieldset>
+                <legend>Description of Duties</legend>
+                <!--<label for="duties-description">Description of Duties:</label>-->
+                <textarea class="form-control" id="duties-description" name="duties-description" rows="8" required></textarea>
+            </fieldset>
+        </div>
+        <input type="submit" class="btn btn-primary pull-right" value="Submit">
+    </form>
 </div>
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script>
+    function isValidStudentID(input) {
+        return input.length == 9 && /\d{9}/.test(input);
+    }
+
     function validateForm() {
-        alert('validation stub');
-        return true;
+        // alert('validation stub');
+
+        var isValidForm = true;
+
+        var plaform = $('#plaform');
+        plaform.find('input[type="text"], input[type="number"], input[type="date"], input[type="email"], textarea').each(function() {
+            isValidForm = isValidForm && ($(this).val().length > 0);
+        });
+
+        isValidForm = isValidForm
+                      && isValidStudentID($('#student-id').val());
+                      // && isValidEmail($)
+
+
+
+        return isValidForm;
     }
 </script>
 </body>
