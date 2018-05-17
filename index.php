51f517e48013fc74afc7dc63cc54398072012ece
h4r3krsna <manju.sreedaran@gmail.com>
2018-05-17 12:43:02 -0700

    DB insertion of POST data

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ function isValidEmail($input) {
 
 function isValidDate($input) {
     $matches = '';
-    preg_match('', $input, $matches);
+    preg_match('/[0-9]{4}-[01][0-9]-[0-3][0-9]/', $input, $matches);
     return (strlen($input) == 10) && (sizeof($matches) == 1);
 }
 
@@ -48,63 +48,83 @@ $f3->route('GET /',
 );
 
 $f3->route('POST /',
-    function() {
+    function($f3) {
         print_r($_POST);
         ln();
 
         foreach($_POST as $key => $value)
         {
-            print_r($value . ' : ' . gettype($value));
-            ln();
-//            if (strstr($key, 'item'))
-//            {
-//                $x = str_replace('item','',$key);
-//                inserttag($value, $x);
-//            }
+//            print_r($value . ' : ' . gettype($value));
+//            ln();
         }
 
-        // validate, sanitize and save to DB
-        $dbConnection = new mysqli("mysql:host=localhost;port=3306;dbname=msreedar_plaform", //$this->f3->get("dbservername"),
-            "msreedar_plaform", //$this->f3->get("dbuser"),
-            "plaform",//$this->f3->get("dbpassword"),
-            "msreedar_plaform");//$this->f3->get("databasename"));
+        if (isValidForm($_POST)) {
 
-        if ($dbConnection->connect_errno) {
-            printf("DB connection failed: %s\n", $dbConnection->connect_error);
-            exit();
+            // validate, sanitize and save to DB
+            $dbConnection = new mysqli("localhost", //$this->f3->get("dbservername"),
+                "msreedar_plaform", //$this->f3->get("dbuser"),
+                "plaform",//$this->f3->get("dbpassword"),
+                "msreedar_plaform");//$this->f3->get("databasename"));
+            if ($dbConnection->connect_errno) {
+                printf("DB connection failed: %s\n", $dbConnection->connect_error);
+                exit();
+            }
+
+            $studentId = $dbConnection->escape_string($_POST["student-id"]);
+            $firstName = $dbConnection->escape_string($_POST["first-name"]);
+            $lastName = $dbConnection->escape_string($_POST["last-name"]);
+            $studentEmail = $dbConnection->escape_string($_POST["student-email"]);
+            $internshipTitle = $dbConnection->escape_string($_POST["internship-title"]);
+            $company = $dbConnection->escape_string($_POST["company"]);
+            $startDate = $dbConnection->escape_string($_POST["start-date"]);
+            $endDate = $dbConnection->escape_string($_POST["end-date"]);
+            $hoursWorked = $dbConnection->escape_string($_POST["hours-worked"]);
+            $supervisorName = $dbConnection->escape_string($_POST["supervisor-name"]);
+            $supervisorTitle = $dbConnection->escape_string($_POST["supervisor-title"]);
+            $supervisorEmail = $dbConnection->escape_string($_POST["supervisor-email"]);
+            $supervisorPhone = $dbConnection->escape_string($_POST["supervisor-phone"]);
+            $dutiesDescription = $dbConnection->escape_string($_POST["duties-description"]);
+
+            // how to get id of last insert https://www.w3schools.com/php/php_mysql_insert_lastid.asp
+            $statement = "INSERT INTO entries (studentid, firstname, lastname, email, internshiptitle, company, startdate,enddate,hoursworked,supervisorname,supervisortitle, supervisoremail,supervisorphone, descriptionofduties) VALUES ("
+                         . "'" . $studentId . "',"
+                         . "'" . $firstName . "',"
+                         . "'" . $lastName . "',"
+                         . "'" . $studentEmail . "', "
+                         . "'" . $internshipTitle . "', "
+                         . "'" . $company . "', "
+                         . "'" . $startDate . "', "
+                         . "'" . $endDate . "', "
+                         . $hoursWorked . ", "
+                         . "'" . $supervisorName . "', "
+                         . "'" . $supervisorTitle . "', "
+                         . "'" . $supervisorEmail . "', "
+                         . "'" . $supervisorPhone . "', "
+                         . "'" . $dutiesDescription . "'"
+                         . ")";
+            echo $statement;
+
+            $entryID = '';
+            if ($dbConnection->query($statement) === TRUE) {
+                $entryID = $dbConnection->insert_id;
+            } else {
+                echo "Error: " . $statement;
+                ln();
+                echo $dbConnection->error;
+            }
+
+            $dbConnection->close();
+
+            echo 'Thank you for your submission!';// . 'Your entry ID is: ' . $entryID;
+
+            // ln(); echo '<a href="http://msreedaran.greenrivertech.net/plaform/entries/1
+
+            // TODO: email the instructor with a link to the entry & return a confirmation to the student
+            $message = "<a href=\"http://msreedaran.greenrivertech.net/plaform/entries/" . $entryID . "\">Click Here</a> to see the details of the form submission.";
+            mail($f3->get("instructorEmail"), "New PLA Request Form Submission", $message);
+        } else {
+            echo "INVALID FORM SUBMISSION DATA";
         }
-
-        /*
-        $sid = $dbConnection->escape_string($_POST["student-id"]);
-        $email = $dbConnection->escape_string($email);
-        $location = $dbConnection->escape_string($location);
-        $phone = $dbConnection->escape_string($phone);
-
-        $name = explode(" ", $name);
-        $first_name = $name[0];
-        $last_name = $name[sizeof($name) - 1];
-        $phone = str_replace("-", "", $phone);
-
-        $statement = "INSERT INTO customer (first_name, last_name, email, location, phone_number) VALUES ('". $first_name ."', '". $last_name ."', '". $email ."', '". $location . "', ";
-        if (strlen($phone) == 10)
-            $statement = $statement . "'" . $phone . "'";
-        else
-            $statement = $statement . "NULL";
-        $statement = $statement . ")";
-        $dbConnection->query($statement);
-*/
-
-        /*
-         * INSERT INTO entries (studentid, firstname, lastname, email, internshiptitle, company, startdate,enddate,hoursworked,supervisorname,supervisortitle, supervisoremail,supervisorphone, descriptionofduties) VALUES ('123456789', 'first','last','email@mail.com','title','company',CURDATE(),CURDATE(),0,'supervisor','boss','boss@work.com','9876542310','duties duties duties duties duties duties duties duties');
-         */
-
-        // if (allOK) {
-        // email the instructor with a link to the entry & return a confirmation to the student
-        // TODO: how to get id of last insert https://www.w3schools.com/php/php_mysql_insert_lastid.asp
-        // echo '<a href="http://msreedaran.greenrivertech.net/plaform/entries/1
-        echo 'Thank you for your submission!';// . 'Your entry ID is: ' . $entryID;
-
-        //} else { return ERRORS; }
     }
 );
 
