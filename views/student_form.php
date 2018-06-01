688d7a028712373e2c1b742e38d4a1674f9be204
h4r3krsna <manju.sreedaran@gmail.com>
2018-06-01 12:53:48 -0500

    'required' attribute for input fields

--- views/student_form.php
+++ views/student_form.php
@@ -32,13 +32,13 @@
                     <fieldset>
                         <legend>Personal Identification</legend>
                         <label for="student-id">Student ID#:</label>
-                        <input type="numberr" class="form-control" id="student-id" name="student-id" placeholder="XXXXXXXXX" requiredd>
+                        <input type="numberr" class="form-control" id="student-id" name="student-id" placeholder="XXXXXXXXX" required>
                         <label for="first-name">First Name:</label>
-                        <input type="text" class="form-control" id="first-name" name="first-name" placeholder="" requiredd">
+                        <input type="text" class="form-control" id="first-name" name="first-name" placeholder="" required">
                         <label for="last-name">Last Name:</label>
-                        <input type="text" class="form-control" id="last-name" name="last-name" placeholder="" requiredd>
+                        <input type="text" class="form-control" id="last-name" name="last-name" placeholder="" required>
                         <label for="student-email">Student Email:</label>
-                        <input type="emaill" class="form-control" id="student-email" name="student-email" placeholder="" requiredd>
+                        <input type="emaill" class="form-control" id="student-email" name="student-email" placeholder="" required>
                     </fieldset>
                 </div>
             </div>
@@ -47,15 +47,15 @@
                     <fieldset>
                         <legend>Internship Information</legend>
                         <label for="internship-title">Internship Title:</label>
-                        <input type="text" class="form-control" id="internship-title" name="internship-title" placeholder="" requiredd>
+                        <input type="text" class="form-control" id="internship-title" name="internship-title" placeholder="" required>
                         <label for="company">Company:</label>
-                        <input type="text" class="form-control" id="company" name="company" placeholder="" requiredd>
+                        <input type="text" class="form-control" id="company" name="company" placeholder="" required>
                         <label for="start-date">Start Date:</label>
-                        <input type="date" class="form-control" id="start-date" name="start-date" requiredd>
+                        <input type="date" class="form-control" id="start-date" name="start-date" required>
                         <label for="end-date">End Date:</label>
-                        <input type="date" class="form-control" id="end-date" name="end-date" requiredd>
+                        <input type="date" class="form-control" id="end-date" name="end-date" required>
                         <label for="hours-worked">Hours Worked:</label>
-                        <input type="numberr" class="form-control" id="hours-worked" name="hours-worked" placeholder="" requiredd>
+                        <input type="numberr" class="form-control" id="hours-worked" name="hours-worked" placeholder="" required>
                     </fieldset>
                 </div>
             </div>
@@ -66,13 +66,13 @@
                     <fieldset>
                         <legend>Supervisor Details</legend>
                         <label for="supervisor-name">Supervisor Name:</label>
-                        <input type="text" class="form-control" id="supervisor-name" name="supervisor-name" placeholder="" requiredd>
+                        <input type="text" class="form-control" id="supervisor-name" name="supervisor-name" placeholder="" required>
                         <label for="supervisor-title">Supervisor Title:</label>
-                        <input type="text" class="form-control" id="supervisor-title" name="supervisor-title" placeholder="" requiredd>
+                        <input type="text" class="form-control" id="supervisor-title" name="supervisor-title" placeholder="" required>
                         <label for="supervisor-email">Supervisor Email:</label>
-                        <input type="emaill" class="form-control" id="supervisor-email" name="supervisor-email" placeholder="" requiredd>
+                        <input type="emaill" class="form-control" id="supervisor-email" name="supervisor-email" placeholder="" required>
                         <label for="supervisor-phone">Supervisor Phone:</label>
-                        <input type="numberr" class="form-control" id="supervisor-phone" name="supervisor-phone" placeholder="XXXXXXXXXX" requiredd>
+                        <input type="numberr" class="form-control" id="supervisor-phone" name="supervisor-phone" placeholder="XXXXXXXXXX" required>
                     </fieldset>
                 </div>
             </div>
@@ -80,7 +80,7 @@
                 <div class="form-group">
                     <fieldset>
                         <legend>Description of Duties</legend>
-                        <textarea class="form-control" id="duties-description" name="duties-description" rows="11" requiredd></textarea>
+                        <textarea class="form-control" id="duties-description" name="duties-description" rows="11" required></textarea>
                     </fieldset>
                 </div>
             </div>
@@ -91,15 +91,15 @@
                     <fieldset>
                         <legend>Additional Questions</legend>
                         <label for="reflection0">What elements and experiences from your coursework best helped you prepare for your internship experience? Your answer may include technical and non-technical elements.</label>
-                        <textarea class="form-control" id="reflection0" name="reflection0" rows="11" requiredd></textarea>
+                        <textarea class="form-control" id="reflection0" name="reflection0" rows="11" required></textarea>
                         <label for="reflection1" class="reflection">What did you learn in your internship experience, either directly at work or independently to support your work? Your answer may include technical and non-technical items.</label>
-                        <textarea class="form-control" id="reflection1" name="reflection1" rows="11" requiredd></textarea>
+                        <textarea class="form-control" id="reflection1" name="reflection1" rows="11" required></textarea>
                         <label for="reflection2" class="reflection">What was your experience with the work environment, company/management culture, and technical/innovation culture? Was it what you expected? And what would you look for in your next work opportunity that is either the same or different from this internship/work experience, from a technical or non-technical perspective?</label>
-                        <textarea class="form-control" id="reflection2" name="reflection2" rows="11" requiredd></textarea>
+                        <textarea class="form-control" id="reflection2" name="reflection2" rows="11" required></textarea>
                         <label for="reflection3" class="reflection">With your internship experience completed, what do you plan on learning going forward into your next courses and/or next work opportunities?</label>
-                        <textarea class="form-control" id="reflection3" name="reflection3" rows="11" requiredd></textarea>
+                        <textarea class="form-control" id="reflection3" name="reflection3" rows="11" required></textarea>
                         <label for="reflection4" class="reflection">With your internship experience completed, what would your recommendations/advice be to students who are searching for their first internship and/or are preparing to start their first internship experience?</label>
-                        <textarea class="form-control" id="reflection4" name="reflection4" rows="11" requiredd></textarea>
+                        <textarea class="form-control" id="reflection4" name="reflection4" rows="11" required></textarea>
                     </fieldset>
                 </div>
             </div>
