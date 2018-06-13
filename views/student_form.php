2e3ad4e5f0f8443661e4b2b8998eabf34be21e86
h4r3krsna <manju.sreedaran@gmail.com>
2018-06-13 06:12:30 -0500

    form layout fixes

--- views/student_form.php
+++ views/student_form.php
@@ -7,9 +7,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <style>
-        .form-group {
-            margin-top: 20px;
-        }
         .reflection {
             margin-top: 25px;
         }
@@ -31,35 +28,53 @@
                 <div class="form-group">
                     <fieldset>
                         <legend>Personal Identification</legend>
-                        <label for="student-id">Student ID#:</label>
-                        <input type="number" class="form-control" id="student-id" name="student-id" placeholder="XXXXXXXXX" required>
-                        <label for="first-name">First Name:</label>
-                        <input type="text" class="form-control" id="first-name" name="first-name" placeholder="" required">
-                        <label for="last-name">Last Name:</label>
-                        <input type="text" class="form-control" id="last-name" name="last-name" placeholder="" required>
-                        <label for="student-email">Student Email:</label>
-                        <input type="email" class="form-control" id="student-email" name="student-email" placeholder="" required>
-                        <label for="student-phone">Student Phone:</label>
-                        <input type="text" class="form-control" id="student-phone" name="student-phone" placeholder="XXXXXXXXXX" required>
+                        <div class="form-group">
+                            <label for="student-id">Student ID#:</label>
+                            <input type="number" class="form-control" id="student-id" name="student-id" placeholder="XXXXXXXXX" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="first-name">First Name:</label>
+                            <input type="text" class="form-control" id="first-name" name="first-name" placeholder="" required">
+                        </div>
+                        <div class="form-group">
+                            <label for="last-name">Last Name:</label>
+                            <input type="text" class="form-control" id="last-name" name="last-name" placeholder="" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="student-email">Student Email:</label>
+                            <input type="email" class="form-control" id="student-email" name="student-email" placeholder="" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="student-phone">Student Phone:</label>
+                            <input type="text" class="form-control" id="student-phone" name="student-phone" placeholder="XXXXXXXXXX" required>
+                        </div>
                     </fieldset>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                <div class="form-group">
-                    <fieldset>
-                        <legend>Internship Information</legend>
-                        <label for="internship-title">Internship Title:</label>
-                        <input type="text" class="form-control" id="internship-title" name="internship-title" placeholder="" required>
-                        <label for="company">Company:</label>
-                        <input type="text" class="form-control" id="company" name="company" placeholder="" required>
-                        <label for="start-date">Start Date:</label>
-                        <input type="date" class="form-control" id="start-date" name="start-date" required>
-                        <label for="end-date">End Date:</label>
-                        <input type="date" class="form-control" id="end-date" name="end-date" required>
-                        <label for="hours-worked">Total Hours Worked:</label>
-                        <input type="number" class="form-control" id="hours-worked" name="hours-worked" placeholder="" required>
-                    </fieldset>
-                </div>
+                <fieldset>
+                    <legend>Internship Information</legend>
+                        <div class="form-group">
+                            <label for="internship-title">Internship Title:</label>
+                            <input type="text" class="form-control" id="internship-title" name="internship-title" placeholder="" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="company">Company:</label>
+                            <input type="text" class="form-control" id="company" name="company" placeholder="" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="start-date">Start Date:</label>
+                            <input type="date" class="form-control" id="start-date" name="start-date" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="end-date">End Date:</label>
+                            <input type="date" class="form-control" id="end-date" name="end-date" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="hours-worked">Total Hours Worked:</label>
+                            <input type="number" class="form-control" id="hours-worked" name="hours-worked" placeholder="" required>
+                        </div>
+                </fieldset>
             </div>
         </div>
         <div class="row">
@@ -67,14 +82,22 @@
                 <div class="form-group">
                     <fieldset>
                         <legend>Supervisor Details</legend>
-                        <label for="supervisor-name">Supervisor Name:</label>
-                        <input type="text" class="form-control" id="supervisor-name" name="supervisor-name" placeholder="" required>
-                        <label for="supervisor-title">Supervisor Title:</label>
-                        <input type="text" class="form-control" id="supervisor-title" name="supervisor-title" placeholder="" required>
-                        <label for="supervisor-email">Supervisor Email:</label>
-                        <input type="email" class="form-control" id="supervisor-email" name="supervisor-email" placeholder="" required>
-                        <label for="supervisor-phone">Supervisor Phone:</label>
-                        <input type="number" class="form-control" id="supervisor-phone" name="supervisor-phone" placeholder="XXXXXXXXXX" required>
+                        <div class="form-group">
+                            <label for="supervisor-name">Supervisor Name:</label>
+                            <input type="text" class="form-control" id="supervisor-name" name="supervisor-name" placeholder="" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="supervisor-title">Supervisor Title:</label>
+                            <input type="text" class="form-control" id="supervisor-title" name="supervisor-title" placeholder="" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="supervisor-email">Supervisor Email:</label>
+                            <input type="email" class="form-control" id="supervisor-email" name="supervisor-email" placeholder="" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="supervisor-phone">Supervisor Phone:</label>
+                            <input type="number" class="form-control" id="supervisor-phone" name="supervisor-phone" placeholder="XXXXXXXXXX" required>
+                        </div>
                     </fieldset>
                 </div>
             </div>
@@ -92,16 +115,26 @@
                 <div class="form-group">
                     <fieldset>
                         <legend>Additional Questions</legend>
-                        <label for="reflection0">What elements and experiences from your coursework best helped you prepare for your internship experience? Your answer may include technical and non-technical elements.</label>
-                        <textarea class="form-control" id="reflection0" name="reflection0" rows="11" required></textarea>
-                        <label for="reflection1" class="reflection">What did you learn in your internship experience, either directly at work or independently to support your work? Your answer may include technical and non-technical items.</label>
-                        <textarea class="form-control" id="reflection1" name="reflection1" rows="11" required></textarea>
-                        <label for="reflection2" class="reflection">What was your experience with the work environment, company/management culture, and technical/innovation culture? Was it what you expected? And what would you look for in your next work opportunity that is either the same or different from this internship/work experience, from a technical or non-technical perspective?</label>
-                        <textarea class="form-control" id="reflection2" name="reflection2" rows="11" required></textarea>
-                        <label for="reflection3" class="reflection">With your internship experience completed, what do you plan on learning going forward into your next courses and/or next work opportunities?</label>
-                        <textarea class="form-control" id="reflection3" name="reflection3" rows="11" required></textarea>
-                        <label for="reflection4" class="reflection">With your internship experience completed, what would your recommendations/advice be to students who are searching for their first internship and/or are preparing to start their first internship experience?</label>
-                        <textarea class="form-control" id="reflection4" name="reflection4" rows="11" required></textarea>
+                        <div class="form-group">
+                            <label for="reflection0">What elements and experiences from your coursework best helped you prepare for your internship experience? Your answer may include technical and non-technical elements.</label>
+                            <textarea class="form-control" id="reflection0" name="reflection0" rows="11" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="reflection1" class="reflection">What did you learn in your internship experience, either directly at work or independently to support your work? Your answer may include technical and non-technical items.</label>
+                            <textarea class="form-control" id="reflection1" name="reflection1" rows="11" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="reflection2" class="reflection">What was your experience with the work environment, company/management culture, and technical/innovation culture? Was it what you expected? And what would you look for in your next work opportunity that is either the same or different from this internship/work experience, from a technical or non-technical perspective?</label>
+                            <textarea class="form-control" id="reflection2" name="reflection2" rows="11" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="reflection3" class="reflection">With your internship experience completed, what do you plan on learning going forward into your next courses and/or next work opportunities?</label>
+                            <textarea class="form-control" id="reflection3" name="reflection3" rows="11" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="reflection4" class="reflection">With your internship experience completed, what would your recommendations/advice be to students who are searching for their first internship and/or are preparing to start their first internship experience?</label>
+                            <textarea class="form-control" id="reflection4" name="reflection4" rows="11" required></textarea>
+                        </div>
                     </fieldset>
                 </div>
             </div>
--- views/student_form_invalid.php
+++ views/student_form_invalid.php
@@ -7,9 +7,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <style>
-        .form-group {
-            margin-top: 20px;
-        }
         .reflection {
             margin-top: 25px;
         }
@@ -32,83 +29,114 @@
     <form id="plaform" name="plaform" onsubmit="return validateForm()" method="POST">
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                <div class="form-group">
-                    <fieldset>
-                        <legend>Personal Identification</legend>
+                <fieldset>
+                    <legend>Personal Identification</legend>
+                    <div class="form-group">
                         <label for="student-id">Student ID#:<span class="validationError"><?php if(!$_POST['ok-student-id']) echo "* (must be 9 digits)"; ?></span></label>
-                        <input type="number" class="form-control <?php if(!$_POST['ok-student-id']) echo "errorField"; ?>" id="student-id" name="student-id" placeholder="XXXXXXXXX" required <?php if($_POST['ok-student-id']) echo "value=\"" . $_POST['student-id'] . "\""; ?>>
+                        <input type="number" class="form-control <?php if(!$_POST['ok-student-id']) echo "errorField"; ?>" id="student-id" name="student-id" placeholder="XXXXXXXXX" required value="<?php if($_POST['ok-student-id']) echo $_POST['student-id']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="first-name">First Name:<span class="validationError"><?php if(!$_POST['ok-first-name']) echo "*"; ?></span></label>
                         <input type="text" class="form-control <?php if(!$_POST['ok-first-name']) echo "errorField"; ?>" id="first-name" name="first-name" placeholder="" required value="<?php if($_POST['ok-first-name']) echo $_POST['first-name']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="last-name">Last Name:<span class="validationError"><?php if(!$_POST['ok-last-name']) echo "*"; ?></span></label>
                         <input type="text" class="form-control <?php if(!$_POST['ok-last-name']) echo "errorField"; ?>" id="last-name" name="last-name" placeholder="" required value="<?php if($_POST['ok-last-name']) echo $_POST['last-name']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="student-email">Student Email:<span class="validationError"><?php if(!$_POST['ok-student-email']) echo "* (must be valid email)"; ?></span></label>
                         <input type="email" class="form-control <?php if(!$_POST['ok-student-email']) echo "errorField"; ?>" id="student-email" name="student-email" placeholder="" required value="<?php if($_POST['ok-student-email']) echo $_POST['student-email']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="student-phone">Student Phone:<span class="validationError"><?php if (!$_POST['ok-student-phone']) echo "* (must be 10 digits)"; ?></span></label>
                         <input type="text" class="form-control <?php if (!$_POST['ok-student-phone']) echo "errorField"; ?>" id="student-phone" name="student-phone" placeholder="XXXXXXXXXX" required value="<?php if($_POST['ok-student-phone']) echo $_POST['student-phone']; ?>">
-                    </fieldset>
-                </div>
+                    </div>
+            </div>
+                </fieldset>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                <div class="form-group">
-                    <fieldset>
-                        <legend>Internship Information</legend>
+                <fieldset>
+                    <legend>Internship Information</legend>
+                    <div class="form-group">
                         <label for="internship-title">Internship Title:<span class="validationError"><?php if(!$_POST['ok-internship-title']) echo "*"; ?></span></label>
                         <input type="text" class="form-control <?php if(!$_POST['ok-internship-title']) echo "errorField"; ?>" id="internship-title" name="internship-title" placeholder="" required value="<?php if($_POST['ok-internship-title']) echo $_POST['internship-title']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="company">Company:<span class="validationError"><?php if(!$_POST['ok-company']) echo "*"; ?></span></label>
                         <input type="text" class="form-control <?php if(!$_POST['ok-company']) echo "errorField"; ?>" id="company" name="company" placeholder="" required value="<?php if($_POST['ok-company']) echo $_POST['company']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="start-date">Start Date:<span class="validationError"><?php if(!$_POST['ok-start-date']) echo "*"; if(!$_POST['ok-dates']) echo " (must be on/before End Date)"; ?></span></label>
                         <input type="date" class="form-control <?php if(!($_POST['ok-start-date'] && $_POST['ok-dates'])) echo "errorField"; ?>" id="start-date" name="start-date" required value="<?php if($_POST['ok-start-date'] && $_POST['ok-dates']) echo $_POST['start-date']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="end-date">End Date:<span class="validationError"><?php if(!$_POST['ok-end-date']) echo "*"; if(!$_POST['ok-dates']) echo " (must be on/after Start Date)"; ?></span></label>
                         <input type="date" class="form-control <?php if(!($_POST['ok-end-date'] && $_POST['ok-dates'])) echo "errorField"; ?>" id="end-date" name="end-date" required value="<?php if($_POST['ok-end-date'] && $_POST['ok-dates']) echo $_POST['end-date']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="hours-worked">Total Hours Worked:<span class="validationError"><?php if(!$_POST['ok-hours-worked']) echo "* (must be a number)"; ?></span></label>
                         <input type="number" class="form-control <?php if(!$_POST['ok-hours-worked']) echo "errorField"; ?>" id="hours-worked" name="hours-worked" placeholder="" required value="<?php if($_POST['ok-hours-worked']) echo $_POST['hours-worked']; ?>">
-                    </fieldset>
-                </div>
+                    </div>
+                </fieldset>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                <div class="form-group">
-                    <fieldset>
-                        <legend>Supervisor Details</legend>
+                <fieldset>
+                    <legend>Supervisor Details</legend>
+                    <div class="form-group">
                         <label for="supervisor-name">Supervisor Name:<span class="validationError"><?php if(!$_POST['ok-supervisor-name']) echo "*"; ?></span></label>
                         <input type="text" class="form-control <?php if(!$_POST['ok-supervisor-name']) echo "errorField"; ?>" id="supervisor-name" name="supervisor-name" placeholder="" required value="<?php if($_POST['ok-supervisor-name']) echo $_POST['supervisor-name']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="supervisor-title">Supervisor Title:<span class="validationError"><?php if(!$_POST['ok-supervisor-title']) echo "*"; ?></span></label>
                         <input type="text" class="form-control <?php if(!$_POST['ok-supervisor-title']) echo "errorField"; ?>" id="supervisor-title" name="supervisor-title" placeholder="" required value="<?php if($_POST['ok-supervisor-title']) echo $_POST['supervisor-title']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="supervisor-email">Supervisor Email:<span class="validationError"><?php if(!$_POST['ok-supervisor-email']) echo "* (must be valid email)"; ?></span></label>
                         <input type="email" class="form-control <?php if(!$_POST['ok-supervisor-email']) echo "errorField"; ?>" id="supervisor-email" name="supervisor-email" placeholder="" required value="<?php if($_POST['ok-supervisor-email']) echo $_POST['supervisor-email']; ?>">
+                    </div>
+                    <div class="form-group">
                         <label for="supervisor-phone">Supervisor Phone:<span class="validationError"><?php if(!$_POST['ok-supervisor-phone']) echo "* (must be 10 digits)"; ?></span></label>
                         <input type="number" class="form-control <?php if(!$_POST['ok-supervisor-phone']) echo "errorField"; ?>" id="supervisor-phone" name="supervisor-phone" placeholder="XXXXXXXXXX" required value="<?php if($_POST['ok-supervisor-phone']) echo $_POST['supervisor-phone']; ?>">
-                    </fieldset>
-                </div>
+                    </div>
+                </fieldset>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                <div class="form-group">
-                    <fieldset>
-                        <legend>Description of Duties</legend>
+                <fieldset>
+                    <legend>Description of Duties</legend>
+                    <div class="form-group">
                         <span class="validationError"><?php if(!$_POST['ok-duties-description']) echo "*"; ?></span>
                         <textarea class="form-control <?php if(!$_POST['ok-duties-description']) echo "errorField"; ?>" id="duties-description" name="duties-description" rows="11" required> <?php if($_POST['ok-duties-description']) echo $_POST['duties-description']; ?> </textarea>
-                    </fieldset>
-                </div>
+                    </div>
+                </fieldset>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12">
-                <div class="form-group">
-                    <fieldset>
-                        <legend>Additional Questions</legend>
+                <fieldset>
+                    <legend>Additional Questions</legend>
+                    <div class="form-group">
                         <label for="reflection0">What elements and experiences from your coursework best helped you prepare for your internship experience? Your answer may include technical and non-technical elements. <span class="validationError"><?php if(!$_POST['ok-reflection0']) echo "*"; ?></span></label>
                         <textarea class="form-control <?php if(!$_POST['ok-reflection0']) echo "errorField"; ?>" id="reflection0" name="reflection0" rows="11" required> <?php if($_POST['ok-reflection0']) echo $_POST['reflection0']; ?> </textarea>
+                    </div>
+                    <div class="form-group">
                         <label for="reflection1" class="reflection">What did you learn in your internship experience, either directly at work or independently to support your work? Your answer may include technical and non-technical items. <span class="validationError"><?php if(!$_POST['ok-reflection1']) echo "*"; ?></span></label>
                         <textarea class="form-control <?php if(!$_POST['ok-reflection1']) echo "errorField"; ?>" id="reflection1" name="reflection1" rows="11" required> <?php if($_POST['ok-reflection1']) echo $_POST['reflection1']; ?> </textarea>
+                    </div>
+                    <div class="form-group">
                         <label for="reflection2" class="reflection">What was your experience with the work environment, company/management culture, and technical/innovation culture? Was it what you expected? And what would you look for in your next work opportunity that is either the same or different from this internship/work experience, from a technical or non-technical perspective? <span class="validationError"><?php if(!$_POST['ok-reflection2']) echo "*"; ?></span></label>
                         <textarea class="form-control <?php if(!$_POST['ok-reflection2']) echo "errorField"; ?>" id="reflection2" name="reflection2" rows="11" required> <?php if($_POST['ok-reflection2']) echo $_POST['reflection2']; ?> </textarea>
+                    </div>
+                    <div class="form-group">
                         <label for="reflection3" class="reflection">With your internship experience completed, what do you plan on learning going forward into your next courses and/or next work opportunities? <span class="validationError"><?php if(!$_POST['ok-reflection3']) echo "*"; ?></span></label>
                         <textarea class="form-control <?php if(!$_POST['ok-reflection3']) echo "errorField"; ?>" id="reflection3" name="reflection3" rows="11" required> <?php if($_POST['ok-reflection3']) echo $_POST['reflection3']; ?> </textarea>
+                    </div>
+                    <div class="form-group">
                         <label for="reflection4" class="reflection">With your internship experience completed, what would your recommendations/advice be to students who are searching for their first internship and/or are preparing to start their first internship experience? <span class="validationError"><?php if(!$_POST['ok-reflection4']) echo "*"; ?></span></label>
                         <textarea class="form-control <?php if(!$_POST['ok-reflection4']) echo "errorField"; ?>" id="reflection4" name="reflection4" rows="11" required> <?php if($_POST['ok-reflection4']) echo $_POST['reflection4']; ?> </textarea>
-                    </fieldset>
-                </div>
+                    </div>
+                </fieldset>
             </div>
         </div>
         <input type="submit" class="btn btn-primary pull-right" value="Submit">
