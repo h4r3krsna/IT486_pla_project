bd005519440888d4b5ebacfa729036f01c4720fc
h4r3krsna <manju.sreedaran@gmail.com>
2018-06-13 16:48:50 -0500

    added an Approve button to the instructor's form submission view

--- views/approval_form.php
+++ views/approval_form.php
@@ -10,9 +10,6 @@ $f3 = \Base::instance();
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <style>
-        .form-group {
-            margin-top: 20px;
-        }
         .reflection {
             margin-top: 25px;
         }
@@ -135,9 +132,15 @@ $f3 = \Base::instance();
                 </fieldset>
             </div>
         </div>
+        <button class="btn btn-primary pull-right" id="btnApprove">Approve</button>
     </form>
 </div>
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script>
+    $('#btnApprove').click(function() {
+        window.location = "<?php echo $f3->get('approvalURL') . $f3->get('PARAMS.id'); ?>";
+    });
+</script>
 </body>
 </html>
